b36f8f88805eacc3290c05da3322ed77a1cffbe1
hienlong <hienlong106@gmail.com>
2025-11-18 11:47:31 +0000

    Fix import paths in routes package for Flask application

--- src/flask_api/routes/__init__.py
+++ src/flask_api/routes/__init__.py
@@ -1,7 +1,7 @@
 """Routes package for Flask application."""
-from routes.health import health_bp
+from flask_api.routes.health import health_bp
 # from routes.rag import rag_bp
-from routes.documents import documents_bp
+from flask_api.routes.documents import documents_bp
 
 __all__ = ['health_bp', 
         #    'rag_bp',
